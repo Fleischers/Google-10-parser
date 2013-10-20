666f0990e2c92f496b384b895b0b1be54e85dc9b
Fleischers <Fleischer458@gmail.com>
2013-10-20 23:46:58 +0300

    quotes

--- src/fcl/html/GoogleTopTenParser.java
+++ src/fcl/html/GoogleTopTenParser.java
@@ -3,7 +3,7 @@ package fcl.html;
 import java.io.FileNotFoundException;
 import java.util.List;
 
-import org.htmlcleaner.*;
+//import org.htmlcleaner.*;
 
 public class GoogleTopTenParser {
 	private static String path = "/home/fleischer/workspace/Google-10-parser/resources/";
@@ -13,20 +13,20 @@ public class GoogleTopTenParser {
 		String htmlFromFile = FileManager.read(path + fileName);
 		//System.out.println(htmlFromFile);
 		
-		HtmlCleaner cleaner = new HtmlCleaner();
-		TagNode node = cleaner.clean(htmlFromFile);
+		//HtmlCleaner cleaner = new HtmlCleaner();
+		//TagNode node = cleaner.clean(htmlFromFile);
 		
-		TagNode[] r = node.getElementsByAttValue("class", "r", true, true);
+		//TagNode[] r = node.getElementsByAttValue("class", "r", true, true);
 		StringBuilder domains = new StringBuilder();
-		for (int i=0; i < r.length; i++) {
+		/*for (int i=0; i < r.length; i++) {
 			List <?> elements = r[i].getAllElementsList(true);
 			System.out.println(elements);
-			TagNode[] a = r[i].getElementsByName("a", true);
+			//TagNode[] a = r[i].getElementsByName("a", true);
 			for (int j=0; j < a.length; j++) {
 				String site = a[j].getAttributeByName("href");
 				domains.append(findDomain(site) + "\n");
 			}
-		}
+		}*/
 		System.out.println(domains);
 		
 	}
